1a81b87d4d675fdfc14a6a2116a3dbfafe74f04f
Ryan O'Day <romanguy722@gmail.com>
2024-03-17 23:31:08 -0400

    Update wordle answer

--- wordle/script.js
+++ wordle/script.js
@@ -4,7 +4,7 @@ const NUMBER_OF_GUESSES = 6;
 let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];
 let nextLetter = 0;
-let rightGuessString = 'jayde';
+let rightGuessString = 'david';
 
 function initBoard() {
     let board = document.getElementById("game-board");
