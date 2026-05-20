8fe86126279d7b51cb6e978c07433ad3e639dfa6
Ryan O'Day <romanguy722@gmail.com>
2026-05-20 11:35:17 -0400

    switch wordle back to random word

--- wordle/script.js
+++ wordle/script.js
@@ -4,7 +4,7 @@ const NUMBER_OF_GUESSES = 6;
 let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];
 let nextLetter = 0;
-let rightGuessString = 'david';
+let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)];
 
 function initBoard() {
     let board = document.getElementById("game-board");
